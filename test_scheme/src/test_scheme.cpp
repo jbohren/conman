4afa2cf181961a82aedc10e9b7f30cfd61018d5c
Rose Wall <rwall3@jhu.edu>
2015-03-11 16:27:40 -0400

    it works

--- test_scheme/src/test_scheme.cpp
+++ test_scheme/src/test_scheme.cpp
@@ -35,23 +35,25 @@
 
 #include <boost/assign/std/vector.hpp>
 
+#include <ros/ros.h>
+
 class IOBlock : public RTT::TaskContext {
 public:
   RTT::InputPort<double> in;
-  RTT::InputPort<double> in_ex;
+  //RTT::InputPort<double> in_ex;
 
-  RTT::OutputPort<double> out1;
-  RTT::OutputPort<double> out2;
+  RTT::OutputPort<double> out;
+  //RTT::OutputPort<double> out2;
 
-  IOBlock(const std::string &name) : RTT::TaskContext(name) {
+  IOBlock(int argc, char** argv, const std::string &name) : RTT::TaskContext(name) {
     this->addPort("in",in);
-    this->addPort("in_ex",in_ex);
+    //this->addPort("in_ex",in_ex);
 
-    this->addPort("out1",out1);
-    this->addPort("out2",out2);
+    this->addPort("out",out);
+    //this->addPort("out2",out2);
 
     conman_hook_ = conman::Hook::GetHook(this);
-    conman_hook_->setInputExclusivity("in_ex",conman::Exclusivity::EXCLUSIVE);
+    //conman_hook_->setInputExclusivity("in_ex",conman::Exclusivity::EXCLUSIVE);
   }
   boost::shared_ptr<conman::Hook> conman_hook_;
 };
@@ -64,51 +66,63 @@ int main(int argc, char** argv) {
   OCL::DeploymentComponent deployer("deployer");
   RTT::Logger::Instance()->setLogLevel(RTT::Logger::Debug);
   deployer.import("rtt_ros");
+  deployer.import("rtt_rosnode");
   deployer.getProvider<RTT::Scripting>("scripting")->eval("ros.import(\"conman\")");
   deployer.getProvider<RTT::Scripting>("scripting")->eval("ros.import(\"conman_ros\")"); 
 
-  //std::string scheme_name = "scheme";
-  //deployer.loadComponent(scheme_name, "conman::Scheme");
-  //conman::Scheme* 
-  //conman::Scheme scheme;
-  //scheme = deployer.getPeer(scheme_name);
-  //RTT::TaskContext scheme = deployer.getPeer(scheme_name);
-  //RTT::TaskContext* scheme_ptr = deployer.getPeer(scheme_name);
-
   conman::Scheme scheme("scheme");
-  //conman::Scheme* scheme_ptr = &scheme;
-  deployer.addPeer(&scheme);
-  //scheme.addPeer(deployer);
-
+  scheme.connectPeers(&deployer);
   scheme.loadService("conman_ros");
+  
 
-  IOBlock iob1("iob1");
-  IOBlock iob2("iob2");
-  IOBlock iob3("iob3");
-  IOBlock iob4("iob4");
-  IOBlock iob5("iob5");
+  //deployer.getProvider<RTT::Scripting>("scripting")->eval("ros.import(\"roscpp\")"); 
+  //ros::NodeHandle nh;
+
+
+  IOBlock iob1(argc, argv, "iob1");
+  IOBlock iob2(argc, argv, "iob2");
+  IOBlock iob3(argc, argv, "iob3");
+  IOBlock iob4(argc, argv, "iob4");
+  IOBlock iob5(argc, argv, "iob5");
   scheme.addBlock(&iob1);
+  //deployer.addPeer(&iob1);
+  //deployer.getProvider<RTT::Scripting>("scripting")->eval("ros.init_node(\'iob1\')"); 
+  //iob1.configure();
+  //iob1.start();
+  //roscpp::init_node(iob1);
+
+  //ros::init(argc, argv, iob1.getName());
+  //ros::NodeHandle nh;
+ 
+
   scheme.addBlock(&iob2);
   scheme.addBlock(&iob3);
   scheme.addBlock(&iob4);
   scheme.addBlock(&iob5);
-  iob1.out1.connectTo(&iob2.in);
-  iob2.out2.connectTo(&iob3.in);
-  iob3.out1.connectTo(&iob4.in);
-  iob4.out1.connectTo(&iob5.in);
-  iob5.out1.connectTo(&iob1.in);
+  iob1.out.connectTo(&iob2.in);
+  iob2.out.connectTo(&iob3.in);
+  iob3.out.connectTo(&iob4.in);
+  iob4.out.connectTo(&iob5.in);
+
  
   scheme.latchConnections("iob5","iob1",true);
+
+  //iob5.out.connectTo(&iob1.in);
 
   std::vector<std::string> execution_order;
   scheme.getExecutionOrder(execution_order);
   std::vector<std::string> &ptr_blocks = execution_order;
 
-
+  //iob1.configure();
+  //iob1.start();
+ 
   scheme.configure();
 
   scheme.start();
 
+  //iob1.configure();
+  //iob1.start();
+ 
   scheme.enableBlocks(ptr_blocks, true, true);
 
   OCL::TaskBrowser browse(&deployer);
@@ -116,3 +130,4 @@ int main(int argc, char** argv) {
 
   return 0;
 }
+//ORO_CREATE_COMPONENT(IOBlock(&iob1))
