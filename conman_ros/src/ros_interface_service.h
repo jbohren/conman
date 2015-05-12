d68cb9fba5e93dbf9ca94626b1d9448a7fd926e1
RoseWall <rwall3@jhu.edu>
2015-05-12 10:47:50 -0400

    Update ros_interface_service.h

--- conman_ros/src/ros_interface_service.h
+++ conman_ros/src/ros_interface_service.h
@@ -63,8 +63,8 @@ namespace conman_ros {
   private:
     conman::Scheme *scheme;
  
-    RTT::OperationCaller<bool(std::vector<std::string>&, std::vector<std::string>&, bool, bool)> getBlocks;
-    RTT::OperationCaller<bool(std::vector<std::string>&, std::vector<std::string>&, bool, bool)> getGroups;
+    RTT::OperationCaller<bool(std::vector<std::string>&)> getBlocks;
+    RTT::OperationCaller<bool()> getGroups;
     RTT::OperationCaller<bool(std::vector<std::string>&, std::vector<std::string>&, bool, bool)> switchBlocks;
 
     rtt_actionlib::RTTActionServer<conman_msgs::GetBlocksAction> get_blocks_action_server_;
